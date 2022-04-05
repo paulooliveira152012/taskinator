70e6fdd7a3fa4161b08e4ebadbdb7d362c6115e8
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-04-05 12:57:29 -0400

    why aren't the tasks changing to different columns?

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector("#page-content");
-
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -26,7 +27,26 @@ var taskFormHandler = function(event) {
     type: taskTypeInput
   };
 
+  // createTaskEl(taskDataObj); it said replace
+
+  var isEdit = formEl.hasAttribute("data-task-id");
+
+  // PUT THIS BELOW `var isEdit = ...` in `taskFormHandler()`
+
+  // has data attribute, so get task id and call function to complete edit process
+  if (isEdit) {
+  var taskId = formEl.getAttribute("data-task-id");
+  completeEditTask(taskNameInput, taskTypeInput, taskId);
+} 
+  // no data attribute, so create object as normal and pass to createTaskEl function
+  else {
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
   createTaskEl(taskDataObj);
+}
+
 };
 
 var createTaskEl = function(taskDataObj) {
@@ -144,3 +164,43 @@ formEl.setAttribute("data-task-id", taskId);
 //Making it clear that the task is in edit mode
 document.querySelector("#save-task").textContent = "Save Task";
 
+//function d --> finish edeting a task
+var completeEditTask = function(taskName, taskType, taskId) {
+  // find the matching task list item
+var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+// set new values
+taskSelected.querySelector("h3.task-name").textContent = taskName;
+taskSelected.querySelector("span.task-type").textContent = taskType;
+
+alert("Task Updated!");
+
+formEl.removeAttribute("data-task-id");
+document.querySelector("#save-task").textContent = "Add Task";
+};
+
+
+
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+//function e --> 
+var taskStatusChangeHandler = function(event) {
+  // get the task item's id
+  var taskId = event.target.getAttribute("data-task-id");
+
+  // get the currently selected option's value and convert to lowercase
+  var statusValue = event.target.value.toLowerCase();
+
+  // find the parent task item element based on the id
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']")
+
+  if (statusValue === "to do") {
+    tasksToDoEl.appendChild(taskSelected);
+  } 
+  else if (statusValue === "in progress") {
+    tasksInProgressEl.appendChild(taskSelected);
+  } 
+  else if (statusValue === "completed") {
+    tasksCompletedEl.appendChild(taskSelected);
+  }
+};
